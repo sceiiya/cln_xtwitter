f7138a7b1d489a9b2a15834ea3acbae3620ce9db
sceiiya <meetscheidj@gmail.com>
2023-11-21 11:39:07 +0800

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-center font-bold text-large'>Getting over it?</h1>
+      <h1 className='text-center font-bold text-large text-brown-700'>Getting over it?</h1>
+      <h2 className='text-center text-red-700'>Nothing to get over eh?</h2>
     </>
   )
 }
