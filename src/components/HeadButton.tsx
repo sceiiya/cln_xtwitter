94aaa67be64cc591494f10ac4461b0a4a4af653c
sceiiya <meetscheidj@gmail.com>
2023-12-05 23:11:11 +0800

    initiate header

--- src/components/HeadButton.tsx
+++ src/components/HeadButton.tsx
@@ -1,10 +1,10 @@
 import { I_HeadButton } from '../types/Interfaces';
 
-function HeadButton({icon, text}: I_HeadButton) {
+function HeadButton({Icon, text}: I_HeadButton) {
     return (
         <>
-            <div className="">
-                <div>{icon}</div>
+            <div className="flex p-4 m-2 hover:bg-slate-300 rounded-xl hover:ease-in-out">
+                <div className='p-1'>{<Icon />}</div>
                 {text}
             </div>
         </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,14 @@ function Header() {
     return (
         <>
             <div>
-                <HeadButton
-                    icon={FaTwitter}
-                    text='Home'
-                />
+                <HeadButton Icon={FaTwitter} text='Home' />
+                <HeadButton Icon={FaTwitter} text='Explore' />
+                <HeadButton Icon={FaTwitter} text='Notifications' />
+                <HeadButton Icon={FaTwitter} text='Messages' />
+                <HeadButton Icon={FaTwitter} text='Lists' />
+                <HeadButton Icon={FaTwitter} text='Communities' />
+                <HeadButton Icon={FaTwitter} text='Profile' />
+                <HeadButton Icon={FaTwitter} text='More' />
             </div>
         </>
     )
