7d2339fafee0c720c15740ffb10ff86bb8df71c0
sceiiya <meetscheidj@gmail.com>
2023-12-06 23:25:18 +0800

    add appropriate icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,27 @@
-import HeadButton from './HeadButton';
-import { FaTwitter } from 'react-icons/fa';
+import HeadButton from './HeadButton'
+import { FaTwitter } from 'react-icons/fa'
+import { FaHouseChimney } from "react-icons/fa6"
+import { IoSearch } from "react-icons/io5"
+import { GoBell } from "react-icons/go"
+import { FaRegRectangleList } from "react-icons/fa6"
+import { IoPeopleOutline } from "react-icons/io5"
+import { FiMail } from "react-icons/fi"
+// import { BsPeople } from "react-icons/bs"
+import { IoPersonOutline } from "react-icons/io5"
+import { CgMoreO } from "react-icons/cg"
 
 function Header() {
     return (
         <>
             <div>
-                <HeadButton Icon={FaTwitter} text='Home' />
-                <HeadButton Icon={FaTwitter} text='Explore' />
-                <HeadButton Icon={FaTwitter} text='Notifications' />
-                <HeadButton Icon={FaTwitter} text='Messages' />
-                <HeadButton Icon={FaTwitter} text='Lists' />
-                <HeadButton Icon={FaTwitter} text='Communities' />
-                <HeadButton Icon={FaTwitter} text='Profile' />
-                <HeadButton Icon={FaTwitter} text='More' />
+                <HeadButton Icon={FaHouseChimney} text='Home' />
+                <HeadButton Icon={IoSearch} text='Explore' />
+                <HeadButton Icon={GoBell} text='Notifications' />
+                <HeadButton Icon={FiMail} text='Messages' />
+                <HeadButton Icon={FaRegRectangleList} text='Lists' />
+                <HeadButton Icon={IoPeopleOutline} text='Communities' />
+                <HeadButton Icon={IoPersonOutline} text='Profile' />
+                <HeadButton Icon={CgMoreO} text='More' />
             </div>
         </>
     )
