c4f27125f597f5c9006ecbdf515028880b515c36
sceiiya <meetscheidj@gmail.com>
2023-12-10 23:41:10 +0800

    add show cheet

---
+++ src/components/ShowCheet.tsx
@@ -0,0 +1,23 @@
+function ShowCheet() {
+
+    return(
+        <>
+            <div>
+                <div>profile</div>
+                <div>name</div>
+                <div>username</div>
+                <div>timestamp</div>
+                <div>text</div>
+
+                <div>
+                    <div>reply</div>
+                    <div>recheet</div>
+                    <div>like</div>
+                    <div>share</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ShowCheet
