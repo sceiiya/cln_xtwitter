7d2dbc258b2436945537369d99772a5ad979e67b
sceiiya <meetscheidj@gmail.com>
2023-12-22 16:58:16 +0800

    add profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+
+function Profile() {
+    return (
+        <>
+            <div className="flex p-4 m-2 hover:bg-slate-300 rounded-full hover:ease-in-out">
+            </div>
+        </>
+    )
+}
+
+export default Profile
